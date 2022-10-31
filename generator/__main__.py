a3c9657ef65af33364402067fdbfe1f3bd83b0f9
neuro <t.cakala@neurosys.com>
2022-10-31 14:37:51 +0100

    Added sampling SNVs from within CN events

--- generator/__main__.py
+++ generator/__main__.py
@@ -11,6 +11,7 @@ from generator.generator.gen import SNVModelGenerator, sample_cells_data
 import argparse
 
 from generator.core.model_data import create_cn_matrix, EventTreeWithCounts
+from generator.statistics.statistics_calculator import StatisticsCalculator
 
 parser = argparse.ArgumentParser(description='Generate CONET SNV tree.')
 parser.add_argument('--sequencing_error', type=float,default=0.001)
@@ -27,70 +28,79 @@ parser.add_argument('--snv_events_per_edge', type=int, default=1,
                     help="average number of SNV events on an edge")
 parser.add_argument('--snv_in_cn_proportion', type=float, default=0.1,
                     help="Average proportion of cn nodes which have SNVs sampled so that they overlap with CN event")
+parser.add_argument('--simulation_dir', type=str, required=False)
+parser.add_argument('--stats_dir', type=str, required=False)
 args = parser.parse_args()
 
 
-class Context(SNVGeneratorContext):
-
-    def __init__(self):
-        pass
-
-    def sequencing_error(self) -> float:
-        return args.sequencing_error
-
-    def read_success_prob(self) -> float:
-        return args.read_success_prob
-
-    def snv_in_cn_proportion(self) -> float:
-        return args.snv_in_cn_proportion
-    def per_allele_coverage(self) -> float:
-        return args.coverage
-
-    def max_cn(self) -> int:
-        return args.max_cn
-
-    def neutral_cn(self) -> int:
-        return args.neutral_cn
-
-    def number_of_bins(self) -> int:
-        return args.bins
-
-    def sample_number_of_snvs_for_edge(self, num_available_snvs: int) -> int:
-        return min(num_available_snvs, np.random.poisson(lam=args.snv_events_per_edge, size=None))
-
-
 if __name__ == "__main__":
-    ctxt = Context()
-    model_generator = SNVModelGenerator(ctxt)
-    model = model_generator.generate_model(args.tree_size)
+    if args.simulation_dir is not None:
+        calc = StatisticsCalculator(args.simulation_dir)
+        res = calc.calculate() + '\n'
+        with open(args.stats_dir, "w") as f:
+            f.write(res)
+    else:
+        class Context(SNVGeneratorContext):
 
-    while not any([np.min(n) == 0.0 for n in model.node_to_cn_profile.values()]):
-        logging.getLogger().error("Failed to generate tree with at least one deletion, trying one more time...")
+            def __init__(self):
+                pass
+
+            def sequencing_error(self) -> float:
+                return args.sequencing_error
+
+            def read_success_prob(self) -> float:
+                return args.read_success_prob
+
+            def snv_in_cn_proportion(self) -> float:
+                return args.snv_in_cn_proportion
+
+            def per_allele_coverage(self) -> float:
+                return args.coverage
+
+            def max_cn(self) -> int:
+                return args.max_cn
+
+            def neutral_cn(self) -> int:
+                return args.neutral_cn
+
+            def number_of_bins(self) -> int:
+                return args.bins
+
+            def sample_number_of_snvs_for_edge(self, num_available_snvs: int) -> int:
+                return min(num_available_snvs, np.random.poisson(lam=args.snv_events_per_edge, size=None))
+
+
+        ctxt = Context()
+        model_generator = SNVModelGenerator(ctxt)
         model = model_generator.generate_model(args.tree_size)
 
-    cells_data, cc_matrix = sample_cells_data(clusters=args.clusters, cluster_size=args.cluster_size, model=model,
-                                              ctxt=ctxt, random_attachment=args.random_attachment)
+        while not any([np.min(n) == 0.0 for n in model.node_to_cn_profile.values()]):
+            logging.getLogger().error("Failed to generate tree with at least one deletion, trying one more time...")
+            model = model_generator.generate_model(args.tree_size)
 
-    raw_cc_matrix_to_CONET_format(cc_matrix, model.tree).to_csv(f"./cc", index=False)
-    cn = create_cn_matrix(cells_data, EventTreeWithCounts(model.tree, model.node_to_cn_profile))
-    numpy.savetxt("./cn", cn)
-    with open("./cluster_sizes", "w") as f:
-        for _ in range(0, args.clusters):
-            f.write(f"{args.cluster_size}\n")
+        cells_data, cc_matrix = sample_cells_data(clusters=args.clusters, cluster_size=args.cluster_size, model=model,
+                                                  ctxt=ctxt, random_attachment=args.random_attachment)
 
-    with open("./snvs_data", "w") as f:
-        breakpoints = model.tree.get_breakpoint_loci()
-        for i in range(0, ctxt.number_of_bins()):
-            smaller = [b for b in breakpoints if b <= i]
-            if smaller:
-                f.write(f"{i};{len(smaller) - 1}\n")
-            else:
-                f.write(f"{i};{-1}\n")
+        raw_cc_matrix_to_CONET_format(cc_matrix, model.tree).to_csv(f"./cc", index=False)
+        cn = create_cn_matrix(cells_data, EventTreeWithCounts(model.tree, model.node_to_cn_profile))
+        numpy.savetxt("./cn", cn)
+        with open("./cluster_sizes", "w") as f:
+            for _ in range(0, args.clusters):
+                f.write(f"{args.cluster_size}\n")
 
-    numpy.savetxt("B", cells_data.b, delimiter=";")
-    numpy.savetxt("D", cells_data.d, delimiter=";")
-    with open(f"./event_tree", "wb") as f:
-        pickle.dump(EventTreeWithCounts(model.tree, model.node_to_cn_profile), f)
+        with open("./snvs_data", "w") as f:
+            breakpoints = model.tree.get_breakpoint_loci()
+            for i in range(0, ctxt.number_of_bins()):
+                smaller = [b for b in breakpoints if b <= i]
+                if smaller:
+                    f.write(f"{i};{len(smaller) - 1}\n")
+                else:
+                    f.write(f"{i};{-1}\n")
 
-    with open(f"./attachment", "wb") as f:
-        pickle.dump(cells_data.attachment, f)
+        numpy.savetxt("B", cells_data.b, delimiter=";")
+        numpy.savetxt("D", cells_data.d, delimiter=";")
+        with open(f"./event_tree", "wb") as f:
+            pickle.dump(EventTreeWithCounts(model.tree, model.node_to_cn_profile), f)
+
+        with open(f"./attachment", "wb") as f:
+            pickle.dump(cells_data.attachment, f)
---
+++ generator/statistics/conet_reader.py
@@ -0,0 +1,119 @@
+from collections import defaultdict
+from pathlib import Path
+
+import networkx as nx
+import numpy
+import pandas
+
+
+class ConetReader:
+    def __init__(self, dir, cc_path: Path):
+        self.cc_path = cc_path
+        self.tree_path = Path(dir) / Path("inferred_tree")
+        self.attachment_path = Path(dir) / Path("inferred_attachment")
+        self.snvs_path = Path(dir) / Path("inferred_snvs")
+        self.cn_path = Path(dir) / Path("inferred_counts")
+        self._tree = None
+        self._attachment = None
+        self._cn = None
+        self._snvs = None
+
+    @property
+    def cell_snv_pairs(self) -> list[tuple[int, int]]:
+        result = []
+        for cell, node in enumerate(self._attachment):
+            path = nx.shortest_path(self.tree, (0, 0), node)
+            for n in path:
+                for snv in self._snvs.get(n, set()):
+                    result.append((cell, snv))
+        return result
+
+    @property
+    def tree(self):
+        return self._tree
+
+    @property
+    def attachment(self):
+        return self._attachment
+
+    @property
+    def cn(self):
+        return self._cn
+
+    @property
+    def snvs(self):
+        return self._snvs
+
+    def load(self):
+        self._tree = self._load_tree()
+        self._attachment = self._load_attachment()
+        self._snvs = self._load_snvs()
+        self._cn = self._load_cn()
+
+    def _load_cn(self):
+        return numpy.loadtxt(str(self.cn_path), delimiter=';', dtype=int)
+
+    def _load_snvs(self) -> dict:
+        cc = pandas.read_csv(str(self.cc_path), sep=",")
+        breakpoints = []
+        for i in range(cc.shape[0]):
+            if cc["candidate_brkp"].iloc[i] > 0.0:
+                breakpoints.append(i)
+
+        result = defaultdict(set)
+        with self.snvs_path.open(mode="r") as f:
+            data = f.read().split('\n')
+            for d in data:
+                if d == '':
+                    continue
+                node, snv = d.split(';')
+                node = eval(node)
+                if node != (0,0):
+                    node = (breakpoints[node[0]], breakpoints[node[1]])
+                snv = int(snv)
+                result[node].add(snv)
+        return result
+
+    def _load_attachment(self) -> list:
+        with self.attachment_path.open(mode="r") as f:
+            data = f.read().split('\n')
+            result = []
+            for d in data:
+                if d == '':
+                    continue
+                d = d.replace('1_', '')
+                cell, num, loci1, loci2 = d.split(';')
+                loci1 = int(loci1)
+                loci2 = int(loci2)
+                if loci2 == loci1:
+                    loci2 = 0
+                    loci1 = 0
+                result.append((loci1, loci2))
+        return result
+
+    def _load_tree(self) -> nx.DiGraph:
+        with self.tree_path.open(mode="r") as f:
+            data = f.read()
+
+            def line_to_edge(line):
+                line = line.replace('1_', '')
+                parent, child = line.split('-')
+                parent = eval(parent)
+                child = eval(child)
+                return parent, child
+
+            lines = data.split('\n')
+            edges = []
+            for line in lines:
+                if line == '':
+                    continue
+                edges.append(line_to_edge(line))
+            graph = nx.DiGraph()
+            graph.add_edges_from(edges)
+            return graph
+
+
+if __name__ == "__main__":
+    c = ConetReader("../../tmp/out")
+    c.load()
+    print("x")
