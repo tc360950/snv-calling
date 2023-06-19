1bfe0450505e925af1a1bcda61413d1cf2d678e3
neuro <t.cakala@neurosys.com>
2023-06-19 13:02:29 +0200

    Minor updates

--- scenarios/plotter.py
+++ scenarios/plotter.py
@@ -1,29 +1,20 @@
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# Load the CSV file into a DataFrame
-file_path = './results_test3'  # Replace with your file path
+file_path = './scenarios/scenario2/results_1'
 df = pd.read_csv(file_path, delimiter=";")
 
-# Check the DataFrame
 print(df.head())
 
 print(df.median())
 
-import pandas as pd
-import matplotlib.pyplot as plt
+df = df[df.scenario.isin(["tree_unknown_brkp_known_0.1", "tree_known_brkp_known", "tree_known_attachment_unknown_brkp_known", "tree_unknown_brkp_unknown_0.1"])]
+
 fig, ax = plt.subplots()
-# Assuming df is your DataFrame
 medians = df.groupby(['scenario'])['cell_snv_recall'].median()
 
-# Create boxplot
 boxplot = df.boxplot(column='cell_snv_precision', by='scenario', ax=ax)
-
-# Get the x position of each box
 positions = range(1, len(medians) + 1)
-
-# Add median lines over each box
 for tick, value in zip(positions, medians):
     boxplot.axhline(value, xmin=(tick-1)/len(medians), xmax=tick/len(medians), color='r', linestyle='dashed', linewidth=2)
 plt.xticks(rotation=25)
